cc12c912687abef7f67c59de7b870e0fe6470673
Vadym Tsummer <62544653+icenfame@users.noreply.github.com>
2022-03-12 14:26:49 +0200

    feat: add game auto-detection on different screens

--- bot.py
+++ bot.py
@@ -1,140 +1,95 @@
-from PIL import ImageGrab, ImageOps, Image
+from PIL import ImageGrab, Image
 import pyautogui
 import keyboard
 import numpy as np
 from time import sleep
 
-# Full window
-xBase = 660
-yBase = 71
-hBase = 969 # div
-# Half window
-xBase = 180
-yBase = 80
-hBase = 959 # div
-# hBase = 731 # canvas
+def detectGame():
+	screen = pyautogui.screenshot()
+	button = pyautogui.locateOnScreen('regions/buttons/play.png') or \
+				pyautogui.locateOnScreen('regions/buttons/replay.png') or \
+				pyautogui.locateOnScreen('regions/buttons/left.png')
 
-# Both
-wBase = 600
+	canvasWidth = 600
 
-def screenshot(i = 0):
-	img = ImageGrab.grab(bbox=(xBase, yBase, xBase + wBase, yBase + hBase))
-	# img.save(f"game/screen_{i}.png")
+	# Left
+	for left in range(button.left.item(), 0, -1):
+		if screen.getpixel((left - 1, button.top.item())) != (255, 255, 255):
+			gameX = left
+			break;
+
+	# Right
+	if screen.getpixel((gameX + canvasWidth - 1, button.top.item())) == (255, 255, 255):
+		gameW = canvasWidth
+
+	# Top
+	for top in range(button.top.item(), 0, -1):
+		if screen.getpixel((gameX - 1, top - 1)) != (51, 51, 51) and screen.getpixel((gameX, top + 1)) == (211, 247, 255):
+			gameY = top;
+			break;
+
+	# Bottom
+	for bottom in range(button.top.item(), screen.height, 1):
+		if screen.getpixel((gameX, bottom)) != (255, 255, 255):
+			gameH = bottom - gameY
+			break;
+
+	return (gameX, gameY, gameW, gameH)
+
+def screenshot(region):
+	img = pyautogui.screenshot(region=region)
 	# img.save("screen.png")
-
 	return img
 
-def checkPlayerPosition(img):
-	if img.getpixel((240, 570)) == (51, 93, 101): # red shirt (207, 70, 59)
+def checkPlayer(region, img):
+	headX = 240
+	headY = 388
+	headColor = (51, 93, 101)
+
+	if img.getpixel((headX, region[3] - headY)) == headColor:
 		return "left"
-	elif img.getpixel((wBase - 240, 570)) == (51, 93, 101):
+	elif img.getpixel((region[2] - headX, region[3] - headY)) == headColor:
 		return "right"
 	else:
 		exit("Player not found")
 
-def checkWood(img, player):
-	y = 461
-	w = 60
-	h = 100
-
-	# 0 up
-	# y = 461
-	# 2 up
-	# y = 360
+def checkWood(region, img, player):
+	woodY = region[3] - 498 # +100 to +2up
+	woodW = 32
+	woodH = 100
 
 	if player == "left":
-		x = 200
+		woodX = 230
 	elif player == "right":
-		x = wBase - 200 - w
+		woodX = region[2] - 230 - woodW
 
-	img = img.crop((x, y, x + w, y + h))
-
+	img = img.crop((woodX, woodY, woodX + woodW, woodY + woodH))
 	arr = np.array(img)
 	arr = arr.reshape((-1, 3))
 
 	if [126, 173, 79] in arr:
 		return player
 
-# Start play
-# TODO
+def main():
+	region = detectGame()
 
-# def checkButton():
-# 	x = xBase + 242
-# 	y = 866
-# 	w = 120
-# 	h = 120
+	while True:
+		if keyboard.is_pressed('esc'):
+			break;
 
-# 	img = ImageGrab.grab(bbox=(x, y, x + w, y + h))
-	# img.save("screen.png")
+		img = screenshot(region)
+		player = checkPlayer(region, img)
+		wood = checkWood(region, img, player)
 
-# def captureGame():
-# 	img = ImageGrab.grab(bbox=(xBase, yBase, xBase + wBase, yBase + hBase))
-# 	img.save("screen.png")
+		if player == "left" and wood == "left":
+			pyautogui.press("right")
+			pyautogui.press("right")
+		elif player == "right" and wood == "right":
+			pyautogui.press("left")
+			pyautogui.press("left")
+		else:
+			pyautogui.press(player)
+			pyautogui.press(player)
 
-
-# print(captureWoodBlock())
-
-# checkButton()
-# captureGame()
-# exit()
-# i = 0
-
-pyautogui.PAUSE = 0.3
-
-# Playing
-while True:
-	# sleep(0.01)
-
-	img = screenshot();
-	player = checkPlayerPosition(img)
-	wood = checkWood(img, player)
-
-	# print("Player:", player, "\tWood:", wood)
-
-	# exit()
-
-	# i += 1
-
-	if player == "left" and wood == "left":
-		pyautogui.press("right")
-		pyautogui.press("right")
-
-		# pyautogui.click(x=560, y=920)
-		# pyautogui.click(x=560, y=920)
-	elif player == "right" and wood == "right":
-		pyautogui.press("left")
-		pyautogui.press("left")
-
-		# pyautogui.click(x=400, y=920)
-		# pyautogui.click(x=400, y=920)
-
-	# elif player == "left" and wood2 == "left":
-	# 	pyautogui.press("left")
-	# 	pyautogui.press("left")
-	# 	pyautogui.press("right")
-	# 	pyautogui.press("right")
-
-	# 	# print("2L 2R")
-	# elif player == "right" and wood2 == "right":
-	# 	pyautogui.press("right")
-	# 	pyautogui.press("right")
-	# 	pyautogui.press("left")
-	# 	pyautogui.press("left")
-
-		# print("2R 2L")
-	else:
-		pyautogui.press(player)
-		pyautogui.press(player)
-
-		# if player == "left":
-		# 	pyautogui.click(x=400, y=920)
-		# 	pyautogui.click(x=400, y=920)
-		# else:
-		# 	pyautogui.click(x=560, y=920)
-		# 	pyautogui.click(x=560, y=920)
-
-
-		# print(f"2{player[0].upper()}")
-
-	if keyboard.is_pressed('esc'):
-		break;
+if __name__ == "__main__":
+	main()
